9dcf01a29327ee55218a62523c5611c69767f39a
OngKaChi <khangcheanong@gmail.com>
2025-02-28 14:53:39 +0800

    Update suppliers.php

--- suppliers.php
+++ suppliers.php
@@ -1,4 +1,5 @@
 <?php
+include 'header.php';
 include 'config.php';
 
 ?>
@@ -15,9 +16,6 @@ include 'config.php';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-
-<body>
-<?php include 'header.php'; ?>
 
 <div class="content">
     <div class="content-box">
